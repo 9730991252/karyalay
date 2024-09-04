b89aaee15f377314ea3b6badf8fdce75f076ffe4
9730991252 <sunilkale101@gmail.com>
2024-09-04 08:37:12 +0530

    ok

--- ajax/views.py
+++ ajax/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 import json
 # Create your views here.
 def indraprastha(request, id):
-    data = Karyalay.objects.values().filter(id=id).first()
-    return JsonResponse(data)
+    data_raw = Karyalay.objects.values().filter(id=id)
+    data=list(data_raw)
+    return JsonResponse(data,safe=False)
     
