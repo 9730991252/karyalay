bad9c0861bc99e5a880a8cb3123ac9128c0a6d43
9730991252 <sunilkale101@gmail.com>
2024-09-06 12:34:22 +0530

    ok

--- ajax/api/views.py
+++ ajax/api/views.py
@@ -1,14 +1,15 @@
 from ajax.api.serializers import *
 from rest_framework.decorators import api_view
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from owner .models import Event
+from django.http import HttpResponse
 
 
 @api_view(['GET'])
 def indraprastha_api(request):
-    if request.method == 'GET':
-        return Response({
-            'status':200,
-            'msg':"hi sunil"
-        })
+    ev = Event.objects.filter(karyalay_id=1)
+    serializer= EventSerializer(ev,many=True)
+    json_data= JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data,content_type='application/json')
          
